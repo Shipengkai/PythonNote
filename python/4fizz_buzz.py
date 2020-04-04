81b4e9f90e02bd38422d1c4a6742d87cc65b7b04
Shipengkai <503884164@qq.com>
2020-04-04 11:47:41 +0800

    4.4

---
+++ python/4fizz_buzz.py
@@ -0,0 +1,16 @@
+def fizz_buzz(input):
+    fizz = not bool(input % 3)
+    buzz = not bool(input % 5)
+    if fizz and buzz:
+        return 'Fizz_Buzz'
+    elif buzz:
+        return 'Buzz'
+    elif fizz:
+        return 'Fizz'
+    return input
+
+
+message = ''
+while message.lower() != 'quit':
+    message = input('> ')
+    print(fizz_buzz(int(message)))
